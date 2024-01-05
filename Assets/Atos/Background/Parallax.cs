506169eca2f73716a62240dd7f4289b70b9af24b
Douglas <rayolpereira2@gmail.com>
2024-01-05 18:58:34 -0300

    Alteração Parallax
    plano de fundo, montanhas e nuvens

--- Assets/Atos/Background/Parallax.cs
+++ Assets/Atos/Background/Parallax.cs
@@ -7,22 +7,25 @@ public class Parallax : MonoBehaviour
     private float _length;
     private float _startPos;
     
-    [SerializeField] private Transform _Cm;
-
-    public float ParalaxEffect;
+    [SerializeField] private Transform _Camera;
+    [SerializeField] Transform _alvo;
+    public float _ParalaxEffect;
+    
 
     // Start is called before the first frame update
     void Start()
-    {
+    { 
         _startPos = transform.position.x;
         _length = GetComponent<SpriteRenderer>().bounds.size.x;
+        _Camera = Camera.main.transform;
+        _alvo = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float RePos = _Cm.transform.position.x * (1 - ParalaxEffect);
-        float Distance = _Cm.transform.position.x * ParalaxEffect;
+        float RePos = _Camera.transform.position.x * (1 - _ParalaxEffect);
+        float Distance = _Camera.transform.position.x * _ParalaxEffect;
         transform.position = new Vector3(_startPos + Distance,transform.position.y, transform.position.z);
 
         if(RePos > _startPos + _length)
