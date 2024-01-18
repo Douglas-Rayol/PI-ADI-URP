65296ab4f07f1ecc1950257cbe3707b974495058
Douglas <rayolpereira2@gmail.com>
2024-01-18 17:05:36 -0300

    Animação cogula
    animação inimigo

--- Assets/Inimigos/Ini-cogumelo/Scripts/Cogula.cs
+++ Assets/Inimigos/Ini-cogumelo/Scripts/Cogula.cs
@@ -2,17 +2,141 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Cogula : MonoBehaviour
 {
+    Rigidbody _rb;
+    Animator _anim;
+    bool _isFacingRight;
+    bool _ataqueOn;
+    float _distPlayer;
+    [SerializeField] Transform _player;
+    [SerializeField] Transform _alvo;
+    [SerializeField] Transform[] _pos;
+    [SerializeField] float[] _distPos;
+    [SerializeField] float[] _velocidade;
+    [SerializeField] float _distPosLimit;
+    [SerializeField] float _distPlayerLimit;
+    [SerializeField] float _moveVelocidade;
+    [SerializeField] bool _isPlayer;
+    [SerializeField] bool _stopPlayer;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody>();
+        _anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        _distPlayer = Vector3.Distance(transform.position, _player.position);
+        _distPos[0] = Vector3.Distance(transform.position, _pos[0].position);
+        _distPos[1] = Vector3.Distance(transform.position, _pos[1].position);
+        if (_ataqueOn == false)
+        {
+            SeguirPlayer();
+            Patrulhamento();
+            MoverparaAlvo();
+        }
+        else
+        {
+            Ataque();
+        }
+        Anim();
+    }
+
+    void Anim()
+    {
+        _anim.SetFloat("Andando", Mathf.Abs(_rb.velocity.x));
+        _anim.SetBool("isPlayer", _isPlayer);
+        _anim.SetBool("Attack", _ataqueOn);
+    }
+
+    void Ataque()
+    {
+        _rb.velocity = new Vector3(0, _rb.velocity.y);
+    }
+
+    void Patrulhamento()
+    {
+        if (_distPos[0] < _distPlayerLimit && _isPlayer == false)
+        {
+            _alvo = _pos[1];
+            _stopPlayer = true;
+        }
+        if (_distPos[1] < _distPlayerLimit && _isPlayer == false)
+        {
+            _alvo = _pos[0];
+            _stopPlayer = true;
+        }
+    }
+
+    void SeguirPlayer()
+    {
+        if (_distPlayer < _distPlayerLimit)
+        {
+            _isPlayer = true;
+            _alvo = _player;
+            _stopPlayer = false;
+            _moveVelocidade = _velocidade[1];
+        }
+        else
+        {
+            _isPlayer = false;
+            if (_stopPlayer == false)
+            {
+                _alvo = _pos[0];
+                _moveVelocidade = _velocidade[0];
+                _stopPlayer = true;
+            }
+        }
+    }
+
+    void MoverparaAlvo()
+    {
+        if (transform.position.x < _alvo.position.x)
+        {
+            _rb.velocity = new Vector3(_moveVelocidade, _rb.velocity.y);
+        }
+        else
+        {
+            _rb.velocity = new Vector3(-_moveVelocidade, _rb.velocity.y);
+        }
+
+        if (_rb.velocity.x < 0 && !_isFacingRight)
+        {
+            Flip();
+        }
+        else if (_rb.velocity.x > 0 && _isFacingRight)
+        {
+            Flip();
+        }
+
+    }
+
+    void Flip()
+    {
+        _isFacingRight = !_isFacingRight;
+        Vector3 theScale = transform.localEulerAngles;
+        theScale.y *= -1;
+        transform.localEulerAngles = theScale;
+    }
+
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            _ataqueOn = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            _ataqueOn = false;
+        }
     }
 }
