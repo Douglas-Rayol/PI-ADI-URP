7efc43f57d2d4014cf733d2d3bb1845373254c76
imoralman <pardal.ric@gmail.com>
2024-02-28 17:30:53 -0300

    pato e particulas
    pato funfando
    particulas aparecendo na layer

--- Assets/Inimigos/Ini-Pato/Scripts/Pato.cs
+++ Assets/Inimigos/Ini-Pato/Scripts/Pato.cs
@@ -1,64 +1,52 @@
 using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Burst.CompilerServices;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Assertions.Must;
+using UnityEngine.UIElements;
 
 public class Pato : MonoBehaviour {
 
     float _inicioRay, _distMax;
-    [SerializeField] float _speedPato, _velociPato;
+    [SerializeField] float _velociPato, _atakPato, _volta;
 
-	[SerializeField] Vector3 _direcaoRay;
-    Rigidbody _rbPato;
+    [SerializeField] Vector3 _direcaoRay;
+    [SerializeField] Transform _direcaoPato;
+    [SerializeField] Rigidbody _rbPato;
 
 
-	public Transform _alvo, _pointCast;
+    public Transform _alvo, _pointCast;
+    [SerializeField] bool _posplayer;
+    [SerializeField] float dist;
+    [SerializeField] float distPlayer;
 
+    void Start() {
+       // myTransform = this.GetComponent<Transform>();
+
+    }
 
     void Update() {
+        dist = Vector3.Distance(_alvo.position, transform.position);
+        if(dist< distPlayer && !_posplayer) {
+        
+            Voltar(_alvo);
+            _velociPato = _velociPato + 1.5f;
+            if (dist < 5) {
+                _posplayer = true;
+                _velociPato = _velociPato - 1.5f;
+            }
 
-        ProcuraPlayer();
- 
-	}
+        } else  {
+            Voltar(_direcaoPato);
+          
+        }
+    }
+    void Voltar(Transform value) {
+        transform.LookAt(value.position);
+        transform.Rotate(new Vector3(0, 90, 0), Space.Self);//correcting the original rotation
+        transform.position = Vector2.MoveTowards(transform.position, value.position, _velociPato * Time.deltaTime);
 
-    private void ProcuraPlayer() {
-
-		//RaycastHit hit =  Physics.Raycast(_pointCast.position,  _pointCast.TransformDirection(Vector3.down), out hit, 10f);
-
-		//if (hit.transform != null) { }
-
-        RaycastHit hit;
-		if (Physics.Raycast(_pointCast.position, _pointCast.TransformDirection(Vector3.down), out hit, 60f))  {
-		float _distancia = Vector3.Distance(_pointCast.position,  hit.point);
-
-		    Debug.DrawRay(_pointCast.position, _pointCast.TransformDirection(Vector3.down) * _distancia, Color.red); //visualiza o raycast
-		    Debug.Log(hit.transform.name);  //mostra com o que o raycast esta colidindo
-
-            if (hit.transform.CompareTag("Player")) {
-                this._alvo = hit.transform;
-
-				transform.DOMove(_alvo.position, _speedPato);
-				/*
-				Vector3 _posicaoAlvo = this._alvo.position;
-				Vector3 _posicaoAtual = this.transform.position;
-				Vector3 _direcao = _posicaoAlvo - _posicaoAtual;
-				_direcao = _direcao.normalized;
-				
-				this._rbPato.velocity = (this._speedPato * _direcao);
-				*/
-
-			} else {
-                this._alvo = null;
-				transform.Translate(Vector3.left * this._velociPato * Time.deltaTime);
-
-			
-
-			}
-        } else {
-            this._alvo = null;
-			transform.Translate(Vector3.left * this._velociPato * Time.deltaTime);
-		}
     }
 }
