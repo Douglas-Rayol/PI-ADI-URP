5893fefde186628a3c284d7d8df0bb884cbeab7f
jotape <jotapesings@gmail.com>
2024-05-15 15:22:21 -0300

    Mecânica de Matemática do Bau

--- Assets/Menu/HudPuzzle/Script/TestCalculo.cs
+++ Assets/Menu/HudPuzzle/Script/TestCalculo.cs
@@ -2,15 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class TestCalculo : MonoBehaviour
 {
 
     [Header("Calculo")]
     [SerializeField] private string[] _calculo;
-    [SerializeField] private string _result;
+
+
+
+
+    [SerializeField] private int _index = 0;
+
+
+    [SerializeField] private int _result;
 
     [SerializeField] private TextMeshProUGUI _textoCalculo;
+    [SerializeField] private TextMeshProUGUI _textoResultado;
+
 
     private void Start()
     {
@@ -18,36 +28,74 @@ public class TestCalculo : MonoBehaviour
         ShuffleString(_calculo);
         _textoCalculo.text = "" + _calculo[0] + " =";
 
+    }
+
+    private void Update()
+    {
 
 
+    }
+
+
+    public void Adiciona()
+    {
+        _index += 1;
+        _textoResultado.text = "" + _index;
+
+
+    }
+
+    public void Retirar()
+    {
+        _index -= 1;
+        if(_index <= 0)
+        {
+            _index = 0;
+        }
+
+        _textoResultado.text = "" + _index;
+
+    }
+
+    public void Resultado()
+    {
         if (_calculo[0] == "2+3")
         {
-            _result = "5";
-            Debug.Log(_result);
+            if(_index == 5)
+            {
+                Debug.Log("Você acertou!");
+            }
+            else
+            {
+                Debug.Log("Você errou!");
+            }
         }
 
         else if (_calculo[0] == "8+2")
         {
-            _result = "10";
-            Debug.Log(_result);
+            if (_index == 10)
+            {
+                Debug.Log("Você acertou!");
+            }
+            else
+            {
+                Debug.Log("Você errou!");
+            }
         }
 
         else if (_calculo[0] == "4+8")
         {
-            _result = "12";
-            Debug.Log(_result);
+            if (_index == 12)
+            {
+                Debug.Log("Você acertou!");
+            }
+            else
+            {
+                Debug.Log("Você errou!");
+            }
         }
-
-
-
-
     }
 
-
-    public void ResultadoDoCalculo()
-    {
-
-    }
 
 
 
