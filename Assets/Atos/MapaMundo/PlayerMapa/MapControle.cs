c965c7794b2d6e607c82b8e77f82414212ce12e2
jotapesings <57123887+jotapesings@users.noreply.github.com>
2024-06-10 16:58:33 -0300

    Add Sistema de Movimento no Mapa

---
+++ Assets/Atos/MapaMundo/PlayerMapa/MapControle.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapControle : MonoBehaviour
+{
+    [SerializeField] public Transform[] _posFase;
+}
---
+++ Assets/Atos/MapaMundo/PlayerMapa/PlayerMap.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Schema;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.InputSystem;
+
+public class PlayerMap : MonoBehaviour
+{
+    [SerializeField] Rigidbody _rb;
+    [SerializeField] Animator _anim;
+    [SerializeField] NavMeshAgent _agentPlayer;
+    [SerializeField] Vector3 _move;
+    [SerializeField] MapControle _mapControle;
+    
+    [SerializeField] int _mudaFase;
+    [SerializeField] bool _checkPos;
+
+    //[SerializeField] bool _bonus1;
+
+    void Start()
+    {
+        _mapControle = Camera.main.GetComponent<MapControle>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AnimacaoPlayerMap();
+
+        
+        float _distance = Vector3.Distance(transform.position, _mapControle._posFase[_mudaFase].position);
+        if (_mudaFase > 0)
+        {
+            _agentPlayer.SetDestination(_mapControle._posFase[_mudaFase].position);
+            
+
+        }
+
+    }
+
+
+
+    public void SetFrente(InputAction.CallbackContext value)
+    {
+
+        if (value.performed && !_checkPos)
+        {
+            
+            _checkPos = true;
+            _mudaFase++;
+            
+
+            if(_mudaFase == 3) //Pula Fase
+            {
+                _mudaFase += 1;
+            }
+
+
+        }
+        else
+        {
+            _checkPos = false;
+        }
+
+
+
+    }
+
+    public void SetTras(InputAction.CallbackContext value)
+    {
+        if (value.performed && !_checkPos)
+        {
+            
+            _checkPos = true;
+            _mudaFase--;
+
+            if(_mudaFase == 3) //Pula Fase
+            {
+                _mudaFase -= 1;
+
+            }
+
+        }
+        else
+        {
+            _checkPos = false;
+
+            
+        }
+
+
+
+    }
+
+
+    private void AnimacaoPlayerMap()
+    {
+        _anim.SetFloat("Input", _agentPlayer.speed);
+    }
+
+
+    private IEnumerator TempoParaMudarFase()
+    {
+        _agentPlayer.speed = 0;
+        yield return new WaitForSeconds(2f);
+        _mudaFase++;
+        yield return new WaitForSeconds(.3f);
+        _agentPlayer.speed = 10;
+
+    }
+
+}
