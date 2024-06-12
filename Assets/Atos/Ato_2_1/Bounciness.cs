c3dfb092155097bb2c6dc9aab806cf03c39e3111
imoralman <pardal.ric@gmail.com>
2024-06-12 17:22:38 -0300

    ato 2 - trampolim
    adicionado trampolins no ato 2

---
+++ Assets/Atos/Ato_2_1/Bounciness.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bounciness : MonoBehaviour
+{
+
+    //Trampolin
+
+    [SerializeField] float force = 10f;
+
+    private void OnTriggerEnter(Collider other) {
+        if(other.CompareTag("Player")){
+            other.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            other.GetComponent<Rigidbody>().AddForce(Vector3.up * force, ForceMode.Impulse) ;
+        }
+    }
+
+}
