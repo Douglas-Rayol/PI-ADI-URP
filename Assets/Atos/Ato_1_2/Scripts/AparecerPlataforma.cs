d559fccf6b0a5ea94cc1677027e34c22d3cbf40e
imoralman <pardal.ric@gmail.com>
2024-09-11 16:38:19 -0300

    roda-gigante

--- Assets/Atos/Ato_1_2/Scripts/AparecerPlataforma.cs
+++ Assets/Atos/Ato_1_2/Scripts/AparecerPlataforma.cs
@@ -1,33 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using DG.Tweening;
+using UnityEngine;
 
 public class AparecerPlataforma : MonoBehaviour{
-
+   
     public GameObject[] _ativarPlataformas;
     private bool _plataformaAtiva = false;
 
 
-    public  void OnTriggerEnter(Collider other){
+
+   public  void OnTriggerEnter(Collider other){
+
+    
         
-        if (other.CompareTag("Player") && !_plataformaAtiva){           
-            AtivaPlataformas();            
-        }
-
-    void AtivaPlataformas(){
-
-            _plataformaAtiva = true;
-
-            foreach (GameObject plataforma in _ativarPlataformas){
-                plataforma.SetActive(true);
-
-  
-            }
-
-        }
-       
+    if (other.CompareTag("Player") && !_plataformaAtiva){           
+        AtivaPlataformas();            
     }
     
 
+    void AtivaPlataformas(){
+
+        _plataformaAtiva = true;
+
+        foreach (GameObject plataforma in _ativarPlataformas){
+        plataforma.SetActive(true);
+       
+        }
+    }
+   }
 }
