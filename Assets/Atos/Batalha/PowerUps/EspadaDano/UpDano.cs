6e95c52b4ded34aab070175065319bc9f6439f92
jotapesings <57123887+jotapesings@users.noreply.github.com>
2024-10-25 12:27:35 -0300

    Sistema de Aumentar o Dano

--- Assets/Atos/Batalha/PowerUps/EspadaDano/UpDano.cs
+++ Assets/Atos/Batalha/PowerUps/EspadaDano/UpDano.cs
@@ -4,5 +4,13 @@ using UnityEngine;
 
 public class UpDano : MonoBehaviour
 {
-    
+    void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            other.gameObject.GetComponent<SpawnTiro>()._tiroPlayer.GetComponent<TiroPadrao>()._dano = 2;
+
+            Destroy(gameObject);
+        }
+    }
 }
